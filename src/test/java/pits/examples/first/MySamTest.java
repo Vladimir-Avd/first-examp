33c5ee825ed4be1d08eaabb4a87b7b9d1274f6a6
Vladimir-Avd <v.avdeev@premiumitsolution.com>
2018-03-23 11:41:00 +0300

    Исправленное ожидание загрузки верхнего меню

--- src/test/java/pits/examples/first/MySamTest.java
+++ src/test/java/pits/examples/first/MySamTest.java
@@ -5,6 +5,8 @@ package pits.examples.first;
 //import org.junit.Test; org.testng.
 //import org.testng
 //import org.testng.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -21,7 +23,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.interactions.Actions;
 
 import static org.junit.Assert.assertFalse;
-import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 
 /**
@@ -35,7 +37,8 @@ public class MySamTest {
 
     @BeforeMethod
     public void setUp() throws Exception {
-        driver = new FirefoxDriver();
+//        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 30);
         driver.manage().window().maximize();
     }
@@ -46,23 +49,23 @@ public class MySamTest {
 // Проверка того что верхнее меню появляется на главной странице
 
         driver.get("https://new.ahml.test");
-
-        driver.findElements(By.cssSelector(".row-date__day")).get(5).click();
-
-        // test git-IDEA
-        WebElement myElem =  driver.findElements(By.cssSelector(".row-date__day")).get(4);
+        //String mValue = ".//div/div[1]/address";
+        String mValue = "(.//*[@class='news-promo__date'])[2]";
+        WebElement myWebEl =  driver.findElement(By.xpath(mValue));
         Actions actions = new Actions(driver);
-        actions.moveToElement(myElem).sendKeys(Keys.ARROW_UP).sendKeys(Keys.ARROW_UP).sendKeys(Keys.ARROW_UP).sendKeys(Keys.ARROW_UP).build().perform();
-        WebElement myWebEl = driver.findElements(By.cssSelector(".main-drop__opener.hidden-lg-down")).get(1);
-        String menuName = myWebEl.getAttribute("textContent");
-        //System.out.println(menuName);
-        Assert.assertTrue(menuName.equals("Медиа"));
+        actions.moveToElement(myWebEl).build().perform();
+// Прокрутка страницы вверх
+        for (int j = 0; j< 5 ;j++) {
+            actions.sendKeys(Keys.ARROW_UP).build().perform();
+        }
 
-        wait.until(elementToBeClickable(myWebEl));
-        // Thread.sleep(5000);
+        mValue = ".head-content_active a[href='/media/'][class= 'main-drop__opener  hidden-lg-down']";
+        myWebEl = wait.until(elementToBeClickable(By.cssSelector(mValue)));
         myWebEl.click();
-
-
+//  Проверка того что страница Медиа загрузилась
+        myWebEl = driver.findElements(By.cssSelector(".col-xs-12 .h_spacer")).get(0);
+        String menuName = myWebEl.getAttribute("textContent");
+        Assert.assertTrue(menuName.equals("Медиа"));
     }
 
     @AfterMethod
